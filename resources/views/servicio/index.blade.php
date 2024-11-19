8d3640a02092330e3f20d91af3033de37ef0fe08
Antonio Gloria <2122100585@soy.utj.edu.mx>
2024-11-19 15:51:55 -0600

    sexto commit

--- resources/views/servicio/index.blade.php
+++ resources/views/servicio/index.blade.php
@@ -1,5 +1,13 @@
 @extends('adminlte::page')
 
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Bootstrap JS (Requiere Popper.js) -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+
+
 @section('content')
     <div class="container">
         <div class="row">
