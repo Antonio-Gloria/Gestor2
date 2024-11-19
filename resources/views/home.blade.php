630afc60c8f95b8b0c2287cc30632ddae34c1cae
Antonio Gloria <2122100585@soy.utj.edu.mx>
2024-11-18 20:10:37 -0600

    cuarto commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,12 +17,12 @@
             Servicios
         </a>
     @else
-        <a href="{{ route('login') }}"
+        <!-- <a href="{{ route('login') }}"
             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
             Iniciar sesión
         </a>
 
-       <!-- @if (Route::has('register'))
+        @if (Route::has('register'))
             <a href="{{ route('register') }}"
                 class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                 Register
